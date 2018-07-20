313e138efd828ded834f9bd75e71888f7c265cbe
Arturo Monsreal <arturo@lisuto.com>
2018-07-20 19:06:43 +0900

    Added credentials for test

--- knexfile.js
+++ knexfile.js
@@ -15,8 +15,8 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'postgres://gjnycjqoeqakrm:f180de86d7f9f7ea8b4e16592c326ac5c755e991390208fa2888ddd1da9ee189@ec2-54-83-33-213.compute-1.amazonaws.com:5432/dbccu8ef4cqr81',
-      user:     'arturo',
-      password: 'admin'
+      user:     'kmzhsfvxdstaxl',
+      password: 'f5dec8a5020030bb83a50daeca810a72564d1ed1ae5835926accac617c3d26fe'
     },
     pool: {
       min: 2,
